dcea5c69bb9fec11a48311cde4331e96942c32b1
bacmarcio <bac.marcio@gmail.com>
2022-05-13 13:30:01 -0300

    contact component criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { ContactManagerComponent } from './component/contact-manager/contact-manager.component';
+import { AddContactComponent } from './component/add-contact/add-contact.component';
+import { EditContactComponent } from './component/edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ContactManagerComponent
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
