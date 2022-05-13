da0e3489037080d8a3d2dbd7cc938350f2b8515c
bacmarcio <bac.marcio@gmail.com>
2022-05-13 12:22:05 -0300

    configuração dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ContactManagerComponent } from './component/contact-manager/contact-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent
   ],
   imports: [
     BrowserModule,
