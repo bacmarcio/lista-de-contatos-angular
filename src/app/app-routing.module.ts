c4d2e4be37609c9dc70453bf31e419436e47d509
bacmarcio <bac.marcio@gmail.com>
2022-05-13 15:24:53 -0300

    rotas configuradas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './component/add-contact/add-contact.component';
+import { ContactManagerComponent } from './component/contact-manager/contact-manager.component';
+import { EditContactComponent } from './component/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './component/view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/contacts/admin', pathMatch: 'full' },
+  { path: 'contacts/admin', component: ContactManagerComponent },
+  { path: 'contacts/add', component: AddContactComponent },
+  { path: 'contacts/edit/:contactId', component: EditContactComponent },
+  { path: 'contacts/view/:contactId', component: ViewContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
