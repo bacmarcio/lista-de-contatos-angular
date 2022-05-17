1486d52646460a154bc6210f57e1b0a3e1d68e1f
bacmarcio <bac.marcio@gmail.com>
2022-05-17 08:41:57 -0300

    configurando server-json

--- src/app/component/view-contact/view-contact.component.ts
+++ src/app/component/view-contact/view-contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Params } from '@angular/router';
+import { IContact } from 'src/app/models/iContact';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contact',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewContactComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+      this.contactId = param.get('contactId');
+    });
+    if (this.contactId) {
+      this.loading = true;
+      this.contactService.getContact(this.contactId).subscribe((data: IContact) => {
+        this.contact = data;
+        this.loading = false;
+      }, (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      })
+    }
   }
 
 }
